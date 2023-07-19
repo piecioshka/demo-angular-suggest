bddd0079e4e813237a874016f0a7eedf8b20a198
Piotr Kowalski <piecioshka@gmail.com>
2023-07-19 22:05:28 +0200

    Generate component Suggest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuggestComponent } from './suggest/suggest.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuggestComponent
   ],
   imports: [
     BrowserModule
